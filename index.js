3152064350e4d061c084062be7190aac116e9763
Matt <matthew.salt@gmail.com>
2017-03-20 07:44:59 +0000

    add clustering

--- index.js
+++ index.js
@@ -1,61 +1,75 @@
-var express = require('express')
-var fs = require('fs')
-var darwin = require('national-rail-darwin')
-var bodyParser = require('body-parser')
+const cluster = require('cluster');
 
-var token = fs.readFileSync('./token.txt','UTF-8')
-console.log('token')
+if (cluster.isMaster) {
+    // const cpuCount = require('os').cpus().length;
+    const cpuCount = 1
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+	cluster.on('exit', function (worker) {
+	    console.log('Worker %d with pid %d died' , worker.id, process.pid);
+	    cluster.fork();
+	});
+} else {
+    console.log('Worker %d running.', process.pid);
+	const express = require('express')
+	const fs = require('fs')
+	const darwin = require('national-rail-darwin')
+	const bodyParser = require('body-parser')
+	const token = fs.readFileSync('./token.txt','UTF-8')
+	const client = new darwin(token)  
+	const app = express()
 
-var client = new darwin(token)  
-var app = express()
-app.set('port', (process.env.PORT || 8081));
-app.use(bodyParser.json());
-app.listen(app.get('port'));
+
+	app.set('port', (process.env.PORT || 8081));
+	app.use(bodyParser.json());
+	app.listen(app.get('port'));
 
 
-app.get('/', function(req,res){
-	res.send('Hello!')
-})
+	app.get('/', function(req,res){
+		res.send('Hello!')
+	})
 
-app.get('/darwin/api/departures/*', function(req, res){
-   console.log('DEPARTURE BOARD')
-   var params = req.params
-   if(params){
-      var path = params[0]
-      var tokens = path.split('/')
-      if(tokens.length < 3){
-         res.status(500).send('Invalid Query Path - not enough parameters {3 expected}')
-      }else{
-         var fromStation = tokens[0].toUpperCase()
-         var filterType = tokens[1].toLowerCase()
-         var filterStation = tokens[2].toUpperCase()
+	app.get('/darwin/api/departures/*', function(req, res){
+	   console.log('DEPARTURE BOARD')
+	   const params = req.params
+	   if(params){
+	      const path = params[0]
+	      const tokens = path.split('/')
+	      if(tokens.length < 3){
+	         res.status(500).send('Invalid Query Path - not enough parameters {3 expected}')
+	      }else{
+	         const fromStation = tokens[0].toUpperCase()
+	         const filterType = tokens[1].toLowerCase()
+	         const filterStation = tokens[2].toUpperCase()
 
-         client.getDepartureBoardWithDetails(fromStation, {filter:filterStation}, function(err, result){
-            if(err){
-                 res.status(500).send(err)
-            }else{
-                res.set('Content-Type', 'application/json');
-                res.send(JSON.stringify(result, null, 2))
-            }
-         })
-      }
-   }
-})
+	         client.getDepartureBoardWithDetails(fromStation, {filter:filterStation}, function(err, result){
+	            if(err){
+	                 res.status(500).send(err)
+	            }else{
+	                res.set('Content-Type', 'application/json');
+	                res.send(JSON.stringify(result, null, 2))
+	            }
+	         })
+	      }
+	   }
+	})
 
-app.get('/darwin/api/servicedetails/*', function(req, res){
-   console.log('SERVICE DETAILS')
-   var params = req.params;
-   if(params){
-      var path = params[0]
-      var tokens = path.split('/')
-      if(tokens.length < 1){
-         res.status(500).send('Invalid query path - not enough paramaters {1 expected}')
-      }else{
-         console.log(params)
-         client.getServiceDetails(tokens[0], function(err, result){
-             res.set('Content-Type', 'application/json');
-             res.send(JSON.stringify(result, null, 2))
-         })
-      }
-   }
-})
+	app.get('/darwin/api/servicedetails/*', function(req, res){
+	   console.log('SERVICE DETAILS')
+	   var params = req.params;
+	   if(params){
+	      const path = params[0]
+	      const tokens = path.split('/')
+	      if(tokens.length < 1){
+	         res.status(500).send('Invalid query path - not enough paramaters {1 expected}')
+	      }else{
+	         console.log(params)
+	         client.getServiceDetails(tokens[0], function(err, result){
+	             res.set('Content-Type', 'application/json');
+	             res.send(JSON.stringify(result, null, 2))
+	         })
+	      }
+	   }
+	})
+}
