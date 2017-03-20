94ea70073d9f62e05d137a2b51e7b5059dc33647
Matt <matthew.salt@gmail.com>
2017-03-20 21:27:36 +0000

    only allow requests that have api key as header or query param

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const cluster = require('cluster');
+const cluster = require('cluster')
 
 if (cluster.isMaster) {
-    // const cpuCount = require('os').cpus().length;
-    const cpuCount = 1
+    const cpuCount = require('os').cpus().length
     for (var i = 0; i < cpuCount; i += 1) {
-        cluster.fork();
+        cluster.fork()
     }
 	cluster.on('exit', function (worker) {
-	    console.log('Worker %d with pid %d died' , worker.id, process.pid);
-	    cluster.fork();
-	});
+	    console.log('Worker %d with pid %d died' , worker.id, process.pid)
+	    cluster.fork()
+	})
 } else {
-    console.log('Worker %d running.', process.pid);
+    console.log('Worker %d running.', process.pid)
 	const express = require('express')
 	const fs = require('fs')
 	const darwin = require('national-rail-darwin')
@@ -20,14 +19,24 @@ if (cluster.isMaster) {
 	const client = new darwin(token)  
 	const app = express()
 
-
-	app.set('port', (process.env.PORT || 8081));
-	app.use(bodyParser.json());
-	app.listen(app.get('port'));
+	var keys = fs.readFileSync('apikeys.txt').toString().split("\n")
+	app.set('port', (process.env.PORT || 8081))
+	app.use(bodyParser.json())
+	app.listen(app.get('port'))
 
 
 	app.get('/', function(req,res){
 		res.send('Hello!')
+	})
+
+	app.use(function(req, res, next){
+		console.log(req.headers)
+		if (keys.indexOf(req.query.key) > -1 || 
+		   (req.headers && req.headers.key && keys.indexOf(req.headers.key) > -1)){
+			next()
+		}else{
+			res.status(400).send('unauthorised')
+		}
 	})
 
 	app.get('/darwin/api/departures/*', function(req, res){
@@ -47,7 +56,7 @@ if (cluster.isMaster) {
 	            if(err){
 	                 res.status(500).send(err)
 	            }else{
-	                res.set('Content-Type', 'application/json');
+	                res.set('Content-Type', 'application/json')
 	                res.send(JSON.stringify(result, null, 2))
 	            }
 	         })
@@ -57,7 +66,7 @@ if (cluster.isMaster) {
 
 	app.get('/darwin/api/servicedetails/*', function(req, res){
 	   console.log('SERVICE DETAILS')
-	   var params = req.params;
+	   var params = req.params
 	   if(params){
 	      const path = params[0]
 	      const tokens = path.split('/')
@@ -66,7 +75,7 @@ if (cluster.isMaster) {
 	      }else{
 	         console.log(params)
 	         client.getServiceDetails(tokens[0], function(err, result){
-	             res.set('Content-Type', 'application/json');
+	             res.set('Content-Type', 'application/json')
 	             res.send(JSON.stringify(result, null, 2))
 	         })
 	      }
